953053af5b35049ffc240187f4c8197c2657d2b6
Melissa Cantu <meli_cantu12@hotmail.com>
2018-09-19 10:46:41 -0500

    changes

--- src/Form.js
+++ src/Form.js
@@ -20,63 +20,110 @@ class Form extends React.Component {
     constructor(props) {
         super(props);
         this.state  = {
-            value: [],
-            name: ''
-            
+            allContacts: [],
+            contact: {
+                namePerson: "myName",
+                phone: "12049201421",
+                email: "@gmail.com",
+                adress: "ajgkeal;gea",
+                city: "Houston",
+                state: "TX",
+                zipcode: "1421421"
+            },
+            name: '',
+            phone: '',
+            email: '',
+            adress: '',
+            city: '',
+            state: '',
+            zipcode: ''
         }
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleChange = this.handleChange.bind(this);
     }
 
     handleSubmit(e){
-        console.log('form submitted ' + this.state.value);
         e.preventDefault();
+
+        this.setState({
+            newcontact: {
+                name: this.state.name,
+                phone: this.state.phone,
+                email: this.state.email,
+                adress: this.state.adress,
+                city: this.state.city,
+                state: this.state.state,
+                zipcode: this.state.zipcode,
+            }
+        }, function(){
+            console.log('form submitted ' + this.state.newcontact.name, this.state.newcontact.phone,
+            this.state.newcontact.phone,
+            this.state.newcontact.email,
+            this.state.newcontact.adress,
+            this.state.newcontact.city );
+
+            let tempContacts = this.state.allContacts;
+            tempContacts.push(this.state.newcontact);
+
+            this.setState({allContacts: tempContacts});
+        })
+        
+        
     }
 
     handleChange(e){
+
         this.setState({value: e.target.value});
     }
 
     render() {
+
+        let constactList = this.state.allContacts.map(contact =>{
+
+            return <li key={contact.name}>{contact.name}</li>
+        })
         const { classes } = this.props;
 
         return (
         <div className={classes.container}>
+    
         <form onSubmit={this.handleSubmit}>
             <FormControl className={classes.formControl}>
             {/* <input type="text" name="nameofPerson" value={this.state.nameOfPerson} onChange={this.handleChange} /> */}
                 <InputLabel htmlFor="name-simple">Name</InputLabel>
-                <Input id="name-simple" onChange={this.handleChange} ref="namePerson" value={this.state.namePerson} />
+                <Input id="name-simple" name="namePerson" onChange={e => this.setState({name: e.target.value} )} />
             </FormControl>
             <FormControl className={classes.formControl} aria-describedby="name-helper-text">
                 <InputLabel htmlFor="name-helper">Email</InputLabel>
-                <Input id="name-helper" onChange={this.handleChange} ref="email" value={this.state.email}/>
+                <Input id="name-helper" onChange={this.handleChange} />
             </FormControl>
             <FormControl className={classes.formControl} >
                 <InputLabel htmlFor="name-simple">Phone Number</InputLabel>
-                <Input id="name-simple" onChange={this.handleChange} ref="phone" value={this.state.phone}/>
+                <Input id="name-simple" onChange={this.handleChange} />
             </FormControl>
             <FormControl className={classes.formControl} >
                 <InputLabel htmlFor="name-simple">Adress</InputLabel>
-                <Input id="name-simple" onChange={this.handleChange} ref="adress" value={this.state.adress}/>
+                <Input id="name-simple" onChange={this.handleChange} />
             </FormControl>
             <FormControl className={classes.formControl} >
                 <InputLabel htmlFor="name-simple">City</InputLabel>
-                <Input id="name-simple" onChange={this.handleChange} ref="city" value={this.state.city}/>
+                <Input id="name-simple" onChange={this.handleChange} />
             </FormControl>
             <FormControl className={classes.formControl} >
                 <InputLabel htmlFor="name-simple">State</InputLabel>
-                <Input id="name-simple" onChange={this.handleChange} ref="state" value={this.state.state}/>
+                <Input id="name-simple" onChange={this.handleChange} />
             </FormControl>
             <FormControl className={classes.formControl} >
                 <InputLabel htmlFor="name-simple">Zip Code</InputLabel>
-                <Input id="name-simple" onChange={this.handleChange} ref="zipcode" value={this.state.zipcode}/>
+                <Input id="name-simple" onChange={this.handleChange} />
             </FormControl>
             <br />
-            <Button type="submit" value="submit" variant="contained" className={classes.button} style={{marginTop: '20px'}}>
+            <Button type="submit" value="submit" variant="contained" onClick={this.handleSubmit.bind(this)} className={classes.button} style={{marginTop: '20px'}}>
                 Submit
             </Button>
         </form>
+
+            {constactList}
         </div>
         );
     }
